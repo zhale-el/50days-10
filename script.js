a5375581ca38150fdc8fe8f5c55b5f73bb520205
zhaleh <zhalehelmi@gmail.com>
2023-07-06 20:23:42 +0330

    completed-async

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 jokeBtn.addEventListener("click", generateJoke);
 
-generateJoke();
+// async
 
-function generateJoke() {
-  fetch("https://icanhazdadjoke.com/", {
+generateJoke();
+async function generateJoke() {
+  const res = await fetch("https://icanhazdadjoke.com/", {
     headers: {
       Accept: "application/json",
     },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke;
-    });
+  });
+  const data = await res.json();
+  joke.innerHTML = data.joke;
 }
+
+// .then
+
+// function generateJoke() {
+//   fetch("https://icanhazdadjoke.com/", {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
